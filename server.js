6f54f745a7637a2c9b5d13e5eb08fcbad757bd72
Burak FIRAT <burakfiratz@gmail.com>
2017-10-07 19:01:51 +0300

    My standart boilerplate for index.html & server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+let express = require('express'),
+		app = express(),
+		server = require('http').createServer(app),
+		io = require('socket.io').listen(server),
+		port = process.env.port || 3000;
+
+server.listen(port, function(){
+	console.log('Listening port: ' + port);
+});
+
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
